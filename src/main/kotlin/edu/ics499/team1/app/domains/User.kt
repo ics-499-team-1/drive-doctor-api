0299d51b8020f418a8d4209b9082f6b65ec245fa
Vontha Chan <vontha.chan@my.metrostate.edu>
2023-02-01 22:47:53 -0600

    created user domain

---
+++ src/main/kotlin/edu/ics499/team1/app/domains/User.kt
@@ -0,0 +1,10 @@
+package edu.ics499.team1.app.domains
+
+data class User(
+    val userId: Int,
+    val name: String,
+    val email: String,
+    val phoneNumber: String,
+    val vehicles: List<Vehicle>,
+    val trips: List<Trip>
+)
