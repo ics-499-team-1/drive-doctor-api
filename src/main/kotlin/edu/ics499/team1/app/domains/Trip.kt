f6bab9bedd6f6b0284f761f2c72183e0d3816f85
Ryan Gallagher <97428947+rjjgallagher@users.noreply.github.com>
2023-02-06 20:26:00 -0600

    Added domain models for Vehicle, UpcomingMaintenance, CompletedMaitenance, Trip

--- src/main/kotlin/edu/ics499/team1/app/domains/Trip.kt
+++ src/main/kotlin/edu/ics499/team1/app/domains/Trip.kt
@@ -1,5 +1,11 @@
 package edu.ics499.team1.app.domains
 
-class Trip {
-
-}
+data class Trip (
+    val mileage: Int,
+    val type: String,
+    val vehicleId: Long,
+    val name: String?,
+    val start: String?,
+    val end: String?,
+    val notes: String?
+)
--- src/main/kotlin/edu/ics499/team1/app/domains/Vehicle.kt
+++ src/main/kotlin/edu/ics499/team1/app/domains/Vehicle.kt
@@ -1,5 +1,15 @@
 package edu.ics499.team1.app.domains
 
-class Vehicle {
-
-}
+data class Vehicle(
+    private val id: Long,
+    private val name: String,
+    private val year: Int,
+    private val make: String,
+    private val trim: String,
+    private val odometer: Int,
+    private val licensePlateNumber: String?,
+    private val vin: String?,
+    private val maintenance: List<UpcomingMaintenance>,
+    private val maintenanceHistory: List<CompletedMaintenance>,
+    private val deactivated: Boolean,
+)
