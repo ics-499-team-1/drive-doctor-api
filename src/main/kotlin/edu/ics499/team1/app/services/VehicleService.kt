71012cf38c6d382c609bdd78f80864d1da5fd63b
Ryan Gallagher <97428947+rjjgallagher@users.noreply.github.com>
2023-02-13 12:13:01 -0600

    Finished the start of the VehicleService

--- src/main/kotlin/edu/ics499/team1/app/services/VehicleService.kt
+++ src/main/kotlin/edu/ics499/team1/app/services/VehicleService.kt
@@ -1,8 +1,9 @@
 package edu.ics499.team1.app.services
 
+import edu.ics499.team1.app.repositiories.VehicleDataSource
 import org.springframework.stereotype.Service
 
 @Service
-class VehicleService {
+class VehicleService(private val dataSource: VehicleDataSource) {
 
 }
--- src/test/kotlin/edu/ics499/team1/app/services/VehicleServiceTest.kt
+++ src/test/kotlin/edu/ics499/team1/app/services/VehicleServiceTest.kt
@@ -1,5 +1,6 @@
 package edu.ics499.team1.app.services
 
+import edu.ics499.team1.app.repositiories.VehicleDataSource
 import edu.ics499.team1.app.repositiories.mock.MockVehicleDataSource
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
@@ -8,4 +9,17 @@ import org.junit.jupiter.api.Test
 
 internal class VehicleServiceTest {
 
+//    private val dataSource: VehicleDataSource
+    //private val vehicleService = VehicleService()
+    @Test
+    fun `should call its data source to retrieve vehicles`() {
+        // given
+
+
+        // when
+       // val vehicles = vehicleService.getVehicles()
+
+        // then
+
+    }
 }
