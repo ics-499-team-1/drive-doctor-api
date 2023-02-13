fcc9689ef6294f8308497e899c40ea67bb7c90f4
Ryan Gallagher <97428947+rjjgallagher@users.noreply.github.com>
2023-02-13 14:25:09 -0600

    finished the starter implementation of the Service classes and included tests

---
+++ src/test/kotlin/edu/ics499/team1/app/services/TripServiceTest.kt
@@ -0,0 +1,5 @@
+package edu.ics499.team1.app.services
+
+import org.junit.jupiter.api.Assertions.*
+
+class TripServiceTest
--- src/test/kotlin/edu/ics499/team1/app/services/UserServiceTest.kt
+++ src/test/kotlin/edu/ics499/team1/app/services/UserServiceTest.kt
@@ -1,10 +1,5 @@
 package edu.ics499.team1.app.services
 
-import edu.ics499.team1.app.repositiories.VehicleDataSource
-import io.mockk.mockk
-
 internal class UserServiceTest {
-    private val dataSource: VehicleDataSource = mockk(relaxed = true)
-
-    private val vehicleService = VehicleService(dataSource)
+    
 }
