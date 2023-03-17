395d742d538c4b8556d64f05feb280e79de29d4a
Ryan Gallagher <97428947+rjjgallagher@users.noreply.github.com>
2023-03-17 16:52:51 -0500

    finalized VehicleService tests

--- src/test/kotlin/edu/ics499/team1/app/services/VehicleServiceTest.kt
+++ src/test/kotlin/edu/ics499/team1/app/services/VehicleServiceTest.kt
@@ -9,7 +9,6 @@ import io.mockk.confirmVerified
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -20,68 +19,75 @@ internal class VehicleServiceTest {
     private val userRepository: UserRepository = mockk(relaxed = true)
 
     private val vehicleService = VehicleService(vehicleRepository, userRepository)
+
+    private val userId = 123
+    private val user = UserEntity(
+        userId = userId,
+        firstName = "John",
+        lastName = "Smith",
+        email = "johnsmith@email.com",
+        phoneNumber = "651-555-4455"
+    )
+
+    private val vehicle = Vehicle(
+        name = "My Car",
+        year = 2022,
+        make = "Toyota",
+        model = "Corolla",
+        trim = "LE",
+        odometer = 5000,
+        licensePlateNumber = "ABC123",
+        vin = "12345678901234567",
+        userId = user.userId
+    )
+
+    private val vehicleEntity = VehicleEntity(
+        name = vehicle.name,
+        year = vehicle.year,
+        make = vehicle.make,
+        model = vehicle.model,
+        trim = vehicle.trim,
+        odometer = vehicle.odometer,
+        licensePlateNumber = vehicle.licensePlateNumber,
+        vin = vehicle.vin,
+        deactivated = false,
+        user = user
+    )
    
     @Test
-    fun `should call its data source to retrieve vehicles`() {
+    fun `getVehicles() works as expected`() {
+        // given
+        val expectedResponse = listOf(vehicleEntity)
+
         // when
-        vehicleService.getVehicles()
+        every { vehicleRepository.findAll() } returns expectedResponse
+        val actualResponse = vehicleService.getVehicles()
 
         // then
         verify(exactly = 1) { vehicleRepository.findAll() }
+        assertEquals(expectedResponse, actualResponse)
+        confirmVerified()
     }
 
     @Test
     fun `createVehicle() works as expected`() {
         // given
-        val user = UserEntity(
-            userId = 123,
-            firstName = "John",
-            lastName = "Smith",
-            email = "johnsmith@email.com",
-            phoneNumber = "651-555-4455"
-        )
-        every { userRepository.getReferenceById(any() ) } returns user
-
-        val vehicle = Vehicle(
-            name = "My Car",
-            year = 2022,
-            make = "Toyota",
-            model = "Corolla",
-            trim = "LE",
-            odometer = 5000,
-            licensePlateNumber = "ABC123",
-            vin = "12345678901234567",
-            userId = user.userId
-        )
-        every { vehicleRepository.save(any<VehicleEntity>()) } returns VehicleEntity(
-            name = vehicle.name,
-            year = vehicle.year,
-            make = vehicle.make,
-            model = vehicle.model,
-            trim = vehicle.trim,
-            odometer = vehicle.odometer,
-            licensePlateNumber = vehicle.licensePlateNumber,
-            vin = vehicle.vin,
-            deactivated = false,
-            user = user
-        )
+        every { vehicleRepository.existsByLicensePlateNumber(vehicle.licensePlateNumber)} returns false
+        every { vehicleRepository.existsByVin(vehicle.vin)} returns false
+        every { userRepository.getReferenceById(userId) } returns user
+        every { vehicleRepository.save(any<VehicleEntity>()) } returns vehicleEntity
+        val expectedResponse = vehicle.toVehicleEntity(user)
 
         // when
-        val result = vehicleService.createVehicle(vehicle)
+        val actualResponse = vehicleService.createVehicle(vehicle)
 
         // then
         verify(exactly = 1) { userRepository.getReferenceById(vehicle.userId)}
-        verify(exactly = 1) { vehicleRepository.save(any<VehicleEntity>()) }
-        assertThat(result.name).isEqualTo(vehicle.name)
-        assertThat(result.year).isEqualTo(vehicle.year)
-        assertThat(result.make).isEqualTo(vehicle.make)
-        assertThat(result.model).isEqualTo(vehicle.model)
-        assertThat(result.trim).isEqualTo(vehicle.trim)
-        assertThat(result.odometer).isEqualTo(vehicle.odometer)
-        assertThat(result.licensePlateNumber).isEqualTo(vehicle.licensePlateNumber)
-        assertThat(result.vin).isEqualTo(vehicle.vin)
-        assertThat(result.deactivated).isFalse
-        assertThat(result.user).isEqualTo(user)
+        verify(exactly = 1) { vehicleRepository.save(vehicle.toVehicleEntity(user)) }
+        verify(exactly = 1) { vehicleRepository.existsByLicensePlateNumber(vehicle.licensePlateNumber) }
+        verify(exactly = 1) {  vehicleRepository.existsByVin(vehicle.vin) }
+        assertEquals(expectedResponse, actualResponse)
+        confirmVerified()
     }
     
     @Test
@@ -89,7 +95,6 @@ internal class VehicleServiceTest {
         // given
         val vehicleId = 1
         val expectedResponse = Unit
-
 
         // when
         val response = vehicleService.deleteVehicle(vehicleId)
