da50d19256cb86d11ab873b5f3a3db163029733d
Parker Talley <ptmn9278@gmail.com>
2023-03-29 19:36:57 -0500

    added cors config

--- src/main/kotlin/edu/ics499/team1/app/AppApplication.kt
+++ src/main/kotlin/edu/ics499/team1/app/AppApplication.kt
@@ -2,10 +2,26 @@ package edu.ics499.team1.app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class AppApplication
 
 fun main(args: Array<String>) {
     runApplication<AppApplication>(*args)
+}
+
+// Allows all network traffic. We will probably want to scope this down and possible implement per control CORS
+// configuration.
+// See this link for more info: https://www.baeldung.com/spring-cors
+
+@Configuration
+@EnableWebMvc
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+    }
 }
