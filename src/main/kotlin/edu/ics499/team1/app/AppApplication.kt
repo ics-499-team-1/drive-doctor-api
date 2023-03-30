96976d277f72055b26e2136cd5294fe473460f42
Parker Talley <ptmn9278@gmail.com>
2023-03-29 19:53:23 -0500

    move global cors config

--- src/main/kotlin/edu/ics499/team1/app/AppApplication.kt
+++ src/main/kotlin/edu/ics499/team1/app/AppApplication.kt
@@ -2,10 +2,24 @@ package edu.ics499.team1.app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class AppApplication
 
 fun main(args: Array<String>) {
     runApplication<AppApplication>(*args)
+}
+
+@Configuration
+@EnableWebMvc
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("GET", "POST", "PATCH", "DELETE")
+    }
 }
