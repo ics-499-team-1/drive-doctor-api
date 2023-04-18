be23b73b9e80587c79959a632174ec416ebf96b6
Parker Talley <ptmn9278@gmail.com>
2023-04-17 19:12:14 -0500

    final commit

--- src/main/kotlin/edu/ics499/team1/app/security/config/SecurityConfiguration.kt
+++ src/main/kotlin/edu/ics499/team1/app/security/config/SecurityConfiguration.kt
@@ -10,7 +10,6 @@ import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
-
 @Configuration
 @EnableWebSecurity
 class SecurityConfiguration(
