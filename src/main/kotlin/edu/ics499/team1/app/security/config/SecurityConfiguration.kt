2c958fbd5ddad36a61bd3e8f4a9b919dd5ef437e
Parker Talley <89427043+SpiderMan-ParkerTalley@users.noreply.github.com>
2023-04-21 20:51:46 -0500

    remove suppress

--- src/main/kotlin/edu/ics499/team1/app/security/config/SecurityConfiguration.kt
+++ src/main/kotlin/edu/ics499/team1/app/security/config/SecurityConfiguration.kt
@@ -10,7 +10,6 @@ import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
-@Suppress("unused")
 @Configuration
 @EnableWebSecurity
 class SecurityConfiguration(
